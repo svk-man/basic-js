730aacbf22e8372fdcaa70e12200fb380430fa4e
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2021-03-19 22:09:29 +0300

    feat: implement dream-team function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (typeof members != "undefined" && members != null && members.length != null && members.length > 0) {
+    let memberFirstLetters = [];
+    for (let member of members) {
+      if (typeof member == 'string') {
+        memberFirstLetters.push(member.trim()[0].toUpperCase());
+      }
+    }
+
+    return memberFirstLetters.sort().join('');
+  }
+
+  return false;
 };
