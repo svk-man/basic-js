bd20746a05918008a766fc5ef2605d7151e20c46
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2021-03-30 13:43:12 +0400

    fix: transform-array function

--- src/transform-array.js
+++ src/transform-array.js
@@ -9,29 +9,28 @@ module.exports = function transform(arr) {
   for (let i = 0; i < arr.length; i++) {
     let element = arr[i];
     if (element == '--discard-next') {
+      result.push(null);
       i++;
-    } else if (element == '--discard-prev') {
-      if (i > 0 && result[result.length - 1] == arr[i-1]) {
-        result.splice(result.length - 1, 1);
-      }
     } else if (element == '--double-next') {
       if (i + 1 < arr.length) {
-        if (arr[i + 2] == '--discard-prev') {
-          result.push(arr[i + 1]);
-          i+=2;
-        } else if (arr[i + 2] == '--double-prev') {
-          result.push(arr[i + 1], arr[i + 1], arr[i + 1]);
-          i+=2;
-        } else {
-          //result.push(arr[i + 1]);
-        }
+        result.push(arr[i + 1]);
+      } 
+    } else if (element == '--discard-prev') {
+      if (result.length > 0) {
+        result.pop();
       }
     } else if (element == '--double-prev') {
-      //result.splice(result.length, 2);
+      if (result.length > 0) {
+        result.push(result[result.length - 1]);
+      }
     } else {
       result.push(element);
     }
   }
+
+  result = result.filter(item => {
+    return item != null;
+  })
 
   return result;
 };
