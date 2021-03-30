756853f828386d389e2c231716bd8a7fe2cd6649
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2021-03-30 17:06:49 +0400

    feat: implement extended-repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,47 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let result = '';
+  let repeatTimes = 1;
+  let separator = '+';
+  let addition = '';
+  let additionRepeatTimes = 0;
+  let additionSeparator = '|';
+
+  if ('repeatTimes' in options) {
+    repeatTimes = options['repeatTimes'];
+  }
+
+  if ('separator' in options) {
+    separator = options['separator'];
+  }
+
+  if ('addition' in options) {
+    addition = options['addition'];
+  }
+
+  if ('additionRepeatTimes' in options) {
+    additionRepeatTimes = options['additionRepeatTimes'];
+  }
+
+  if ('additionSeparator' in options) {
+    additionSeparator = options['additionSeparator'];
+  }
+
+  if (additionRepeatTimes > 0) {
+    let tmpStr = (addition + additionSeparator).repeat(additionRepeatTimes);
+    result += tmpStr.substr(0, tmpStr.length - additionSeparator.length);
+  } else {
+    if (addition != '') {
+      result += addition;
+    }
+  }
+
+  if (repeatTimes > 0) {
+    let tmpStr = (str + result + separator).repeat(repeatTimes);
+    result = tmpStr.substr(0, tmpStr.length - separator.length);
+  }
+
+  return result;
 };
   
